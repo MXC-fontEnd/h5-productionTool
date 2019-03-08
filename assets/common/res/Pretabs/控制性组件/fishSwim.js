fbcf1e14ffbe21cee7b4572b592a132b2777dccb
ydlx <lichong536@163.com>
2019-03-08 13:53:51 +0800

    1.鱼游泳 --增加 重复游;2.新增shader效果

--- assets/common/res/Pretabs/控制性组件/fishSwim.js
+++ assets/common/res/Pretabs/控制性组件/fishSwim.js
@@ -12,6 +12,11 @@ cc.Class({
             displayName: "向右游"
         },
 
+        swimRepeat: {
+            default: false,
+            displayName: "重复游"
+        },
+
         swimTime: {
             default: 1,
             type: cc.Integer,
@@ -27,28 +32,57 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
     onLoad() {
+        // 可视区尺寸
+        this.canvasView = cc.find("Canvas");
+        this.viewWidth = this.canvasView.x;
+        this.viewHeight = this.canvasView.y;
+
+        // 鱼的故乡
+        this.fishOriginPos = cc.v2(this.node.x,this.node.y);
+
         this.init();
     },
 
     init() {
         let action;
         if (this.swimLeft) {
-            if (this.destination && this.destination.x !== 0 || this.destination && this.destination.y !== 0) {
-                action = cc.moveTo(this.swimTime, cc.v2(this.destination.x, this.destination.y));
-            } else {
-                action = cc.moveTo(this.swimTime, cc.v2(-530, this.node.y));
-            }
+            action = this.swimToLeft();
         } else if (this.swimRight) {
-            if (this.destination && this.destination.x !== 0 || this.destination && this.destination.y !== 0) {
-                action = cc.moveTo(this.swimTime, cc.v2(this.destination.x, this.destination.y));
-            } else {
-                action = cc.moveTo(this.swimTime, cc.v2(530, this.node.y));
-            }
+            action = this.swimToRight();
         }
 
-        if (action) {
-            this.node.runAction(action);
+        if(this.swimRepeat){
+            action = this.swimToRepeat(action);
         }
+        this.node.runAction(action);
+    },
+
+    // 往左游
+    swimToLeft() {
+        return this.swimTo(cc.v2(-(this.viewWidth / 2 + 100), this.node.y));
+    },
+
+    // 往右游
+    swimToRight() {
+        return this.swimTo(cc.v2(this.viewWidth / 2 + 100, this.node.y));
+    },
+
+    // 游
+    swimTo(pos){
+        let swim = this.destination ? cc.v2(this.destination.x, this.destination.y) : pos;
+        let action2 = cc.moveTo(this.swimTime, swim);
+        let action1 = cc.callFunc(function(){
+            this.node.setPosition(this.fishOriginPos);
+        }, this);
+        let action3 = cc.sequence(action1,action2);
+        return action3;
+    },
+
+    // 重复游
+    swimToRepeat(swim){
+        return cc.repeatForever(swim);
     }
+
+
     // update (dt) {},
 });
